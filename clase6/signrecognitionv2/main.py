61c1f24f020bd62ab840b846249957c1d0f22de0
lpanonymous <lpanonymous0101@gmail.com>
2024-04-14 14:18:24 -0600

    captura de siluetas

--- clase6/signrecognitionv2/main.py
+++ clase6/signrecognitionv2/main.py
@@ -1,19 +1,44 @@
 import os
 import numpy as np
 from PIL import Image
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras import models, layers
+from keras.layers import Dropout
+from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 
-# Definir el diccionario de gestos y el mapeo de gestos
-gestures = {'L_': 'L', 'fi': 'Fist', 'ok': 'Okay', 'pe': 'Peace', 'pa': 'Palm'}
-gestures_map = {'Fist': 0, 'L': 1, 'Okay': 2, 'Palm': 3, 'Peace': 4}
+gestures = {'L_': 'L',
+           'fi': 'Fist',
+           'ok': 'Okay',
+           'pe': 'Peace',
+           'pa': 'Palm'
+            }
+
+gestures_map = {'Fist': 0,
+                'L': 1,
+                'Okay': 2,
+                'Palm': 3,
+                'Peace': 4
+                }
+
 
 def process_image(path):
     img = Image.open(path)
     img = img.resize((224, 224))
     img = np.array(img)
     return img
+
+
+def process_data(X_data, y_data):
+    X_data = np.array(X_data, dtype='float32')
+    if rgb:
+        pass
+    else:
+        X_data = np.stack((X_data,)*3, axis=-1)
+    X_data /= 255
+    y_data = np.array(y_data)
+    y_data = to_categorical(y_data)
+    return X_data, y_data
+
 
 def walk_file_tree(relative_path):
     X_data = []
@@ -28,52 +53,72 @@ def walk_file_tree(relative_path):
                 y_data.append(gestures_map[gesture_name])
             else:
                 continue
-                
+    X_data, y_data = process_data(X_data, y_data)
     return X_data, y_data
 
-# Ruta de las imágenes
+
+class Data(object):
+    def __init__(self):
+        self.X_data = []
+        self.y_data = []
+
+    def get_data(self):
+        return self.X_data, self.y_data
+
+
 relative_path = 'C:/Users/zS22000728/Documents/InterfacesNaturalesDeUsuario/clase6/signrecognitionv2/silhouettes'
+rgb = False
 
-# Obtener los datos
 X_data, y_data = walk_file_tree(relative_path)
-
-# Convertir a arrays numpy
-X_data = np.array(X_data, dtype='float32') / 255.0
-y_data = np.array(y_data)
-
-# Dividir en conjuntos de entrenamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2, random_state=12, stratify=y_data)
 
-# Ajustar la forma de los datos de entrada
-X_train = np.expand_dims(X_train, axis=-1)  # Agregar dimensión de canal
-X_test = np.expand_dims(X_test, axis=-1)  # Agregar dimensión de canal
-
-# Construir el modelo
-model = Sequential()
-model.add(Conv2D(32, (5, 5), strides=(2, 2), activation='relu', input_shape=(224, 224, 1)))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Flatten())
-model.add(Dense(units=128, activation='relu'))
-model.add(Dense(units=128, activation='relu'))
-model.add(Dense(units=128, activation='relu'))
+model = models.Sequential()
+model.add(layers.Conv2D(32, (5, 5), strides=(2, 2), activation='relu', input_shape=(224, 224, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(128, activation='relu'))
 model.add(Dropout(0.25, seed=21))
-model.add(Dense(units=5, activation='softmax'))
+model.add(layers.Dense(5, activation='softmax'))
 
-# Compilar el modelo
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-# Entrenar el modelo
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
 model.fit(X_train, y_train, epochs=4, batch_size=64, validation_data=(X_test, y_test), verbose=1)
 
-# Evaluar el modelo
-_, accuracy = model.evaluate(X_test, y_test)
-print('Accuracy: %.2f' % (accuracy * 100))
+# evaluate the keras model
+_, accuracy = model.evaluate(X_data, y_data)
+print('Accuracy: %.2f' % (accuracy*100))
+# make class predictions with the model
+#predictions = (model.predict(X_data) > 0.5).astype(int)
+# summarize the first 5 cases
+#for i in range(5):
+#    print('{} (expected {})'.format(predictions[i], y_data[i]))
 
-# Guardar el modelo
-model.save('C:/Users/zS22000728/Documents/InterfacesNaturalesDeUsuario/clase6/signrecognitionv2/modelo.hdf5')
-print('Modelo guardado exitosamente.')
+file_path = 'C:/Users/zS22000728/Documents/InterfacesNaturalesDeUsuario/clase6/signrecognitionv2/saved_model.hdf5'
+# Guardar el Modelo
+model.save(file_path)
+
+
+# Recrea el mismo modelo  desde el archivo
+from keras.models import load_model
+loaded_model = load_model(file_path)
+
+gesture_names = {0: 'Fist',
+                 1: 'L',
+                 2: 'Okay',
+                 3: 'Palm',
+                 4: 'Peace'}
+
+for i in range(5):
+    pred_array = loaded_model.predict(X_data[i].reshape(1, 224, 224, 3))
+    #print(f'pred_array: {pred_array}')
+    result = gesture_names[np.argmax(pred_array)]
+    #print(f'Result: {result}')
+    #print(max(pred_array[0]))
+    score = float("%0.2f" % (max(pred_array[0]) * 100))
+    print('{} (score {})'.format(result, score))
 
---
+++ clase7/signcapture.py
@@ -0,0 +1,92 @@
+import cv2
+import numpy as np
+import time
+
+# General Settings
+action = ''
+score = 0
+img_counter = 500
+i=0
+
+# Turn on/off the ability to save images
+save_images, selected_gesture = True, 'peace'
+
+# parameters
+cap_region_x_begin = 0.5  # start point/total width
+cap_region_y_end = 0.8  # start point/total width
+threshold = 60  # binary threshold
+blurValue = 41  # GaussianBlur parameter
+bgSubThreshold = 50
+learningRate = 0
+
+# variableslt
+isBgCaptured = 0  # bool, whether the background captured
+triggerSwitch = False  # if true, keyboard simulator works
+
+
+def remove_background(frame):
+    fgmask = bgModel.apply(frame, learningRate=learningRate)
+    kernel = np.ones((3, 3), np.uint8)
+    fgmask = cv2.erode(fgmask, kernel, iterations=1)
+    res = cv2.bitwise_and(frame, frame, mask=fgmask)
+    return res
+
+
+# Camera
+camera = cv2.VideoCapture(0)
+camera.set(10, 200)
+
+while camera.isOpened():
+    ret, frame = camera.read()
+    frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
+    frame = cv2.flip(frame, 1)  # flip the frame horizontally
+    cv2.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0),
+                  (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
+
+    cv2.imshow('camara', frame)
+
+    # Run once background is captured
+    if isBgCaptured == 1:
+        img = remove_background(frame)
+        img = img[0:int(cap_region_y_end * frame.shape[0]),
+              int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]  # clip the ROI
+        # cv2.imshow('mask', img)
+
+        # convert the image into binary image
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (blurValue, blurValue), 0)
+        # cv2.imshow('blur', blur)
+        ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # Add prediction and action text to thresholded image
+        # cv2.putText(thresh, f"Prediction: {prediction} ({score}%)", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
+        # cv2.putText(thresh, f"Action: {action}", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))  # Draw the text
+        # Draw the text
+        #cv2.putText(thresh, f"Prediction: {prediction} ({score}%)", (50, 30), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                   # (255, 255, 255))
+        #cv2.putText(thresh, f"Action: {action}", (50, 80), cv2.FONT_HERSHEY_SIMPLEX, 1,
+        #            (255, 255, 255))  # Draw the text
+        cv2.imshow('background', thresh) #mandar este frame para capturar el gesto en imwrite
+
+    # Keyboard OP
+    k = cv2.waitKey(10)
+    if k == 27:  # press ESC to exit all windows at any time
+        break
+    elif k == ord('b'):  # press 'b' to capture the background
+        bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
+        time.sleep(2)
+        isBgCaptured = 1
+        print('Background captured')
+    elif k == ord('r'):  # press 'r' to reset the background
+        time.sleep(1)
+        bgModel = None
+        triggerSwitch = False
+        isBgCaptured = 0
+        print('Reset background')
+    elif k == ord('c'):
+        # If c pressed
+        target = np.stack((thresh,) * 3, axis=-1)
+        target = cv2.resize(target, (224, 224))
+        target = target.reshape(1, 224, 224, 3)
+        cv2.imwrite(f"C:/Users/zS22000728/Documents/InterfacesNaturalesDeUsuario/clase7/silhouettes/P_{str(i).zfill(3)}.jpg", thresh)
+        i+=1
+
